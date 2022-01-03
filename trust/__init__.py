3f5417c0f536ebf724dba10d11176ff9c852991f
grimadas <bulat.nasrulin@gmail.com>
2022-01-03 19:46:25 +0100

    Reorganize trust init

--- trust/__init__.py
+++ trust/__init__.py
@@ -1,9 +1,9 @@
-from .bartercast import BarterCast, RawBarterCast, PenaltyCast, BoundedBarterCast, MaxFlow
+from .maxflow_based import BarterCast, RawBarterCast, PenaltyCast, BoundedBarterCast, MaxFlow
 from .hitting_time import PersonalizedHittingTime, BiasedPHT
-from .netflow import Netflow
+from .maxflow_based import Netflow
 from .pagerank import PersonalizedPageRank
 from .connectivity import LocalConnectivity
-from .trustrank import TrustRank
+from .pagerank import TrustRank
 from .random_walks import RandomWalks, BiasStrategies
-from .reciprocal_scaled_pagerank import ReciprocalScaledPageRank, SBPPR, WBPPR
-from .reciprocal_scaled_bounded_hitting_time import ReciprocalScaledBoundedHittingTime, RSBHTBiased
+from .pagerank import ReciprocalScaledPageRank, SBPPageRank, WBPPageRank
+from .hitting_time import RSBHittingTime, BiasedRSBHittingTime
